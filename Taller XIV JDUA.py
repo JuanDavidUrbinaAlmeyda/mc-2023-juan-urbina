c098c086e704e9e37014a299a323b52054baf93d
JuanDavidUrbinaAlmeyda <116216282+JuanDavidUrbinaAlmeyda@users.noreply.github.com>
2023-03-29 07:22:46 -0500

    Add files via upload

---
+++ Taller XIV JDUA.py
@@ -0,0 +1,64 @@
+import copy
+
+def imprimirSistema(a, b, etiqueta):
+    n = len(b)
+    print(etiqueta)
+    for i in range(n):
+        for j in range(n):
+            print(a[i][j], end = " ")
+        print("|", b[i])
+    print()
+
+def gaussJordan(ao, bo):
+    a = copy.deepcopy(ao)
+    b = copy.copy(bo)
+
+    n = len(b)
+    
+    imprimirSistema(a, b, "Matriz inicial")
+    for i in range(n):
+        #Pivoteo-------------------------------------------------------------
+        if a[i][i]==0:
+            #Cambiar Ecuación
+            for t in range (i+1,n):
+                if a[t][i]!=0:
+                    a[i], a[t] = a[t], a[i]
+                    b[i], b[t] = b[t], b[i]
+                    break
+
+        pivote = a[i][i]
+
+        #Dividir por el pivote-----------------------------------------------------
+        for j in range(n):
+            a[i][j] /= pivote
+
+        b[i] /= pivote
+        imprimirSistema(a, b, "División")
+
+        #Reducción
+        for k in range(n):
+            if i != k:
+                #Se reduce
+                valorAux = -a[k][i]
+                for j in range(n):
+                    a[k][j] += a[i][j] * valorAux
+                b[k] += b[i] * valorAux
+        imprimirSistema(a, b, "Reducción")
+    
+    return b
+
+a = [[1, 1, 0], [3, 3, 4], [4, 1, 0]]
+b = [2.5, 11.5, 15]
+x = gaussJordan(a, b)
+
+print("Respuesta:")
+for i in range(len(x)):
+    print("x" + str(i+1), "=", x[i])
+
+#Pruebas
+print("\nPruebas:")
+for i in range(len(b)):
+    valorAux = b[i]
+    for j in range(len(b)):
+        valorAux -= a[i][j] * x[j]
+    print("Test", i + 1, "=", valorAux)
